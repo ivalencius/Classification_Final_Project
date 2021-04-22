ca3c3ed421da66068d94d301ac84215951751573
ivalencius <valencig@bc.edu>
2021-04-22 01:12:16 -0400

    cropped shp files

--- Classification/preprocessing.m
+++ Classification/preprocessing.m
@@ -11,22 +11,54 @@ info = shapeinfo("Recent_Large_Fire_Perimeters__Last_Five_Years__2015_2019____gt
 
 %% Dereference shapefile -> set project projection
 proj = info.CoordinateReferenceSystem;
-CA_SHP_lon = [CA_SHP.X];
-CA_SHP_lat = [CA_SHP.Y];
-[CA_SHP_lon,CA_SHP_lat] = projinv(proj,CA_SHP_lon,CA_SHP_lat);
 
-%% Test Visualization of data
+%% Crop SHP file to Area of Interest
+bounding_lon = [-124, -122];
+bounding_lat = [38, 40];
+bbox = [bounding_lat(1), bounding_lon(1); bounding_lat(2), bounding_lon(2)];
+
+%% Extract and project bounding boxes for polygons onto proper projection
+% bbox = left, bottom, right, top
+CA_bboxes = {CA_SHP.BoundingBox};
+idx_outside = NaN(size(CA_bboxes));
+for i = 1:length(CA_bboxes)
+   tmp = CA_bboxes{i};
+   [tmp(1,1), tmp(1,2)] = projinv(proj, tmp(1,1), tmp(1,2));
+   [tmp(2,1), tmp(2,2)] = projinv(proj, tmp(2,1), tmp(2,2));
+   if tmp(1,1) < bbox(1,1)
+       tmp(1,1) = NaN;
+       idx_outside(i) = 1; % Null index found
+   end
+   if tmp(1,2) < bbox(1,2)
+       tmp(1,2) = NaN;
+       idx_outside(i) = 1; % Null index found
+   end
+   if tmp(2,1) > bbox(2,1)
+       tmp(2,1) = NaN;
+       idx_outside(i) = 1; % Null index found
+   end
+   if tmp(2,2) > bbox(2,2)
+       tmp(2,2) = NaN;
+       idx_outside(i) = 1; % Null index found
+   end
+   CA_bboxes{i} = [tmp(1,1), tmp(1,2), tmp(2,1), tmp(2,2)];
+end
+
+idx_outside = find(idx_outside == 1);
+CA_SHP(idx_outside) = [];
+
+%% Dereference shapefile -> set project projection
+CA_SHP_lon = [CA_SHP.Y];
+CA_SHP_lat = [CA_SHP.X];
+
+[CA_SHP_lat,CA_SHP_lon] = projinv(proj,CA_SHP_lat,CA_SHP_lon);
 figure (1); clf
-geoplot(CA_SHP_lon,CA_SHP_lat)
+geoplot(CA_SHP_lat,CA_SHP_lon)
 hold on
 geobasemap('streets')
 
-%% Crop to Area of Interest
-bounding_lon = [38, 40];
-bounding_lat = [-124, -122];
-[bounding_lon,bounding_lat] = projfwd(proj,bounding_lon,bounding_lat);
-% bbox = left, bottom, right, top
-bbox = [bounding_lon(1), bounding_lat(1); bounding_lon(2), bounding_lat(2)];
-% Indexes of shapes outside bounding box
-CA_bboxes = CA_SHP.BoundingBox;
-[idx_out, ~] = find(CA_bboxes(1) > bbox(1));
+%% Test Visualization of data
+figure (1); clf
+geoplot(CA_SHP_lat,CA_SHP_lon)
+hold on
+geobasemap('streets')
